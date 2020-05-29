0e3cb9042a5dc973e621dbad84f47f704ec2234c
Jinit Jain <jinitmjain15698@gmail.com>
2020-05-29 18:51:13 +0530

    Appbar panel color changed

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -26,12 +26,12 @@ export default function ButtonAppBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="static" style={{background:"#009688"}} >
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6" className={classes.title} >
             Investor Buddy
           </Typography>
           <LoginDialogBox />
