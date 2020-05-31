2810f4ea84f0152131238441d5f61cd27290b3fb
Jinit Jain <jinitmjain15698@gmail.com>
2020-05-31 17:52:46 +0530

    npm run build executed

---
+++ build/precache-manifest.f35202a6ed3db6954a58c2b1dbde030d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68aecbd6fc1237a88702f047fcb6a4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d3cd66808e3fa65ba4b",
+    "url": "/static/css/main.bd801996.chunk.css"
+  },
+  {
+    "revision": "7945d962cc0a6b035fb9",
+    "url": "/static/js/2.484c6a3a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.484c6a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d3cd66808e3fa65ba4b",
+    "url": "/static/js/main.39f71a4f.chunk.js"
+  },
+  {
+    "revision": "59622facc166d6cefe6d",
+    "url": "/static/js/runtime-main.8b7f9d2b.js"
+  },
+  {
+    "revision": "2bf40ab72ea4897a3fd9b6e48b233a19",
+    "url": "/static/media/Garamond_Regular.2bf40ab7.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "de6a214e131f2b3237164d5593b5a7a2",
+    "url": "/static/media/news.de6a214e.jpg"
+  }
+]);
