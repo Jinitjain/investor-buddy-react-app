b1a14f66002eec8f609066734cb6155258618589
Pratik Prabhu <prabhupratik27@gmail.com>
2020-05-31 12:05:33 +0530

    Added request to make relation between user and companies

--- src/components/MultiselectList.js
+++ src/components/MultiselectList.js
@@ -6,6 +6,8 @@ import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import {Box} from "@material-ui/core";
+import companies from '../data/companyDatafinal.json'
+import axios from 'axios'
 
 const animatedComponents = makeAnimated();
 
@@ -24,21 +26,28 @@ export default function AnimatedMultiselectList() {
 
   const classes = useStyles();
 
-  var companies = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' },
-    { value: 'chocolate1', label: 'Chocolate1' },
-    { value: 'strawberry2', label: 'Strawberry2' },
-    { value: 'vanilla3', label: 'Vanilla3' },
-  ];
-
   const [state, setState] = React.useState({
     selectedList: ''
   });
 
-  const handleSubmit = () => {
-        console.log('onSumbit: ', state.selectedList.selectedOption);
+  const handleSubmit = async () => {
+      let companiesArray = []
+        // console.log('onSumbit: ', state.selectedList.selectedOption);
+        state.selectedList.selectedOption.forEach(obj => {
+            console.log(obj.value+'.NS')
+            companiesArray.push(obj.value+'.NS')
+        })
+
+        console.log(companiesArray)
+
+      const response = await axios.post('https://webappsvc-investor-buddy.azurewebsites.net/users/makeRelation', {
+          user: 'Jinit',
+          org: companiesArray
+      })
+
+      if(response.status === 200) {
+          console.log("Done")
+      }
     };
 
   const handleChange = selectedOption => {  
